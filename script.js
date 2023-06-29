661d1ac615df281054976b1aff2e3a86f3261d7b
Krupal Malakiya <krupalmalakiya@gmail.com>
2023-06-29 10:55:28 +0530

    face recognition working

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
 const imageUpload = document.getElementById("imageUpload");
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
-])
-  .then(start)
-  .catch((err) => {
-    console.log(err);
-  });
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+]).then(start);
 
 async function start() {
   const container = document.createElement("div");
-  const labeledDescriptors = await loadLabeledImages();
-  const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
   container.style.position = "relative";
+  // container.style.width = "40px";
+  // container.style.height = "40px";
   document.body.append(container);
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.5);
+  let image;
+  let canvas;
   document.body.append("Loaded");
-
   imageUpload.addEventListener("change", async () => {
-    const image = await faceapi.bufferToImage(imageUpload.files[0]);
+    if (image) image.remove();
+    if (canvas) canvas.remove();
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
     container.append(image);
-    const canvas = faceapi.createCanvasFromMedia(image);
+    canvas = faceapi.createCanvasFromMedia(image);
     container.append(canvas);
-    const displaySize = {
-      width: image.width,
-      height: image.height,
-    };
+    const displaySize = { width: image.width, height: image.height };
     faceapi.matchDimensions(canvas, displaySize);
     const detections = await faceapi
       .detectAllFaces(image)
       .withFaceLandmarks()
       .withFaceDescriptors();
-
-    const resizedDetection = faceapi.resizeResults(detections, displaySize);
-    const results = resizedDetection.map((d) =>
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const results = resizedDetections.map((d) =>
       faceMatcher.findBestMatch(d.descriptor)
     );
     results.forEach((result, i) => {
-      const box = resizedDetection[i].detection.box;
+      const box = resizedDetections[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, {
         label: result.toString(),
       });
+      console.log(
+        "------------------------IMAGE NAME:----------",
+        result.toString()
+      );
       drawBox.draw(canvas);
     });
-
-    document.body.append(detections.length);
   });
 }
 
 function loadLabeledImages() {
-  const lables = [
+  const labels = [
     "Aditya",
-    "Anil",
-    // "Archan",
-    // "Gabriel_bheekar",
-    // "Krupal",
-    // "Ritesh",
-    // "Satish",
-    // "Yash Fullstack",
-    // "Yash hingu",
+    // "Anila",
+    "Archan",
+    "Gabriel_bheekar",
+    "Krupal",
+    "Ritesh",
+    "Satish",
+    "Yash Fullstack",
+    "Yash hingu",
   ];
-
   return Promise.all(
-    lables.map(async (lable) => {
-      const discriptions = [];
+    labels.map(async (label) => {
+      const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(
-          `https://github.com/KrupalMalakiya/faceRecognition_JavaScript/tree/master/images/${lable}/${i}.jpg`
-        );
+        const img = await faceapi.fetchImage(`./images/${label}/${i}.jpg`);
 
-        const detection = await faceapi
+        const detections = await faceapi
           .detectSingleFace(img)
           .withFaceLandmarks()
-          .withFaceDescriptors();
+          .withFaceDescriptor();
 
-        discriptions.push(detection.descriptor);
+        descriptions.push(detections?.descriptor);
+
+        console.log("Image :", label, " No: ", i, "IMAGE DATA");
+        console.log("Detections:", detections);
       }
-      return new faceapi.LabeledFaceDescriptors(lable, discriptions);
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
   );
 }
